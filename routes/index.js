6bdaa2f0972e74052fbb2eb171e002dcbd03e177
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-10-07 00:07:52 +0200

    improve(google_analytics): add google analytics to result and error pages

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,8 @@ router.post('/invite', function (req, res) {
           sendMessageToChannel(`${req.body.email}: invited`);
           res.render('result', {
             community: config.community,
-            message: 'Success! Check &ldquo;' + req.body.email + '&rdquo; for an invite from Slack.'
+            message: 'Success! Check &ldquo;' + req.body.email + '&rdquo; for an invite from Slack.',
+            config
           });
         } else {
           let error = body.error;
@@ -61,7 +62,8 @@ router.post('/invite', function (req, res) {
             res.render('result', {
               community: config.community,
               message: 'Success! You were already invited.<br>' +
-                'Visit <a href="https://' + config.slackUrl + '">' + config.community + '</a>'
+                'Visit <a href="https://' + config.slackUrl + '">' + config.community + '</a>',
+              config
             });
             return;
           } else if (error === 'invalid_email') {
@@ -73,7 +75,8 @@ router.post('/invite', function (req, res) {
           res.render('result', {
             community: config.community,
             message: 'Failed! ' + error,
-            isFailed: true
+            isFailed: true,
+            config
           });
         }
       });
@@ -124,7 +127,8 @@ router.post('/invite', function (req, res) {
     res.render('result', {
       community: config.community,
       message: 'Failed! ' + errMsg.join(' and ') + '.',
-      isFailed: true
+      isFailed: true,
+      config
     });
   }
 });
